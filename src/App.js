c10e9830a0ebda13f4bbea2b18b37c092218061b
Adrian Pilarczyk <adrian@useo.pl>
2020-03-26 18:29:49 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+}
 
 function App() {
+  const [form, setForm] = useState(initialValues)
+
+  const handleFieldChange = (e) => {
+    const { name, value } = e.target
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text={form.firstName} second_text="halo" third_text="witaj" />
+      <input value={form.firstName} name="firstName"  placeholder='First Name' onChange={handleFieldChange} />
+      <input value={form.lastName} name="lastName" placeholder='Last Name' onChange={handleFieldChange} />
     </div>
   );
 }
