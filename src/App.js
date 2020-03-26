8668cb0d4f0a93d163506ee28738dd42f136b7a7
Adrian Pilarczyk <adrian@useo.pl>
2020-03-26 18:47:44 +0100

    Add comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 
@@ -9,6 +9,7 @@ const initialValues = {
 
 function App() {
   const [form, setForm] = useState(initialValues)
+  const [value, setValue] = useState("")
 
   const handleFieldChange = (e) => {
     const { name, value } = e.target
@@ -19,11 +20,30 @@ function App() {
     });
   }
 
+  useEffect(() => {
+    // componentDidUpdate
+  })
+
+  useEffect(() => {
+    // formDidUpdate
+  }, [form])
+
+  useEffect(() => {
+    // componentDidMount // willMount
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      // componentWillUnmount
+    }
+  }, [])
+
   return (
     <div className="App">
       <Header text={form.firstName} second_text="halo" third_text="witaj" />
       <input value={form.firstName} name="firstName"  placeholder='First Name' onChange={handleFieldChange} />
       <input value={form.lastName} name="lastName" placeholder='Last Name' onChange={handleFieldChange} />
+      <input value={value} placeholder='Osobny' onChange={(e) => {setValue(e.target.value)}} />
     </div>
   );
 }
